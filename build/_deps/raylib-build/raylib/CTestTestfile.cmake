01e00e8fde13df33cf0d4b7a664d9cb25d30cc3f
Aryanlivi <aryanlivi@gmail.com>
2021-08-02 12:09:51 +0545

    Raylib Practice

---
+++ build/_deps/raylib-build/raylib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Aryan/Raylib_Projects/Snake/build/_deps/raylib-src/src
+# Build directory: D:/Aryan/Raylib_Projects/Snake/build/_deps/raylib-build/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
